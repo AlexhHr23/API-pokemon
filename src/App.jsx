99d4c74ebd711456cef7e3e4458f1a438bd56745
Alexhr <alex.hernandez24kb@gmail.com>
2024-01-21 22:37:28 -0600

    implementado iconos de react icons

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/Button"
+import './sass/App.scss'
+
+
+const App = () => {
+    return(
+        <> 
+            {/*tarjetas*/}
+            <div className="buttons-container">
+                <Button text="Anterior"/>
+                <Button text="Siguiente"/>
+            </div>
+        </>
+    )
+}
+
+export {App}
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import '../sass/Button.scss';
+const Button = (props) => {
+  return (
+    <div className='button_box'>
+        <button className="button">{props.text}</button>
+    </div>
+  )
+}
+
+export {Button};
